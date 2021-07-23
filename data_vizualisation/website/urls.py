3ff4da129f810f29ccd449ef99d6a490c57f7486
Hayas92 <sayah99@hotmail.fr>
2021-07-24 00:37:20 +0200

    data vizualisation update 1

--- data_vizualisation/website/urls.py
+++ data_vizualisation/website/urls.py
@@ -1,6 +1,12 @@
-from django.urls import path
-from . import views
+from django.contrib import admin
+from django.urls import path, include
 
 urlpatterns = [
-    path('', views.home, name="home"),
+    path('admin/', admin.site.urls),
+    path('', include('animes.urls')),
 ]
+
+# Configure Admin Titles
+admin.site.site_header = "My Club Administration Page"
+admin.site.site_title = "Browser Title"
+admin.site.index_title = "Welcome To The Addmin Area..."
